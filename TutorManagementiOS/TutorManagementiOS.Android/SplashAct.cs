802f31ec79f4a0fa8474ab7ebc49bf596b9b7a0d
Justin matte <crakdaple386@hotmail.com>
2021-11-23 11:54:14 -0500

    Add project files.

---
+++ TutorManagementiOS/TutorManagementiOS.Android/SplashAct.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TutorManagementiOS.Droid
+{
+    [Activity(Label = "SplashAct", Icon = "@mipmap/icon", NoHistory = true, Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class SplashAct : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Thread.Sleep(200);
+            Finish();
+        }
+    }
+}
