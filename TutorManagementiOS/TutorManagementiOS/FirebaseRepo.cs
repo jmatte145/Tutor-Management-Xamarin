6dd568bfbbf4705998ab1e697d2c0d41e6a1fc36
Justin matte <crakdaple386@hotmail.com>
2021-11-26 13:02:32 -0500

    Added User Class and began work on Firebase Repo.

---
+++ TutorManagementiOS/TutorManagementiOS/FirebaseRepo.cs
@@ -0,0 +1,56 @@
+﻿using Firebase.Database;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorManagementiOS
+{
+    public class FirebaseRepo
+    {
+        FirebaseClient firebaseClient =
+    new FirebaseClient("https://studentxamarinproject-default-rtdb.firebaseio.com/");
+
+        public async Task<bool> SaveUser(User user)
+        {
+            var data = await firebaseClient.Child(nameof(User)).
+                PostAsync(JsonConvert.SerializeObject(user));
+
+            if (!string.IsNullOrEmpty(data.Key))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            return (await firebaseClient.
+                Child(nameof(User)).OnceAsync<User>()).Select(
+                item => new User
+                {
+                    firstName = item.Object.firstName,
+                    lastName = item.Object.lastName,
+                    userName = item.Object.userName,
+                    password = item.Object.password,
+                }).ToList();
+        }
+
+        public async Task<User> GetByUserId(string userID)
+        {
+            return (await firebaseClient.Child(nameof(User) + "/" + userID).OnceSingleAsync<User>());
+        }
+
+        public async Task<bool> UpdateUser(User user)
+        {
+            await firebaseClient.Child(nameof(User) + "/" + user.userID).PutAsync(JsonConvert.SerializeObject(user));
+            return true;
+        }
+    }
+}
+
---
+++ TutorManagementiOS/TutorManagementiOS/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TutorManagementiOS
+{
+    public class User
+    {
+        public int userID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
