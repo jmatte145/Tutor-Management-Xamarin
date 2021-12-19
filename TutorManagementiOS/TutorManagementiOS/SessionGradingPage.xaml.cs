6d0d9c664aeb3bb8f81c5531642bab70d4ce998b
DmitryGrigoryev <grigoryev.dima@gmail.com>
2021-12-19 15:29:26 -0500

    teacher stuff

---
+++ TutorManagementiOS/TutorManagementiOS/SessionGradingPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TutorManagementiOS
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SessionGradingPage : ContentPage
+    {
+        FirebaseRepo db = new FirebaseRepo();
+        public SessionGradingPage()
+        {
+            InitializeComponent();
+            displayUser();
+        }
+        async void displayUser()
+        {
+            List<SessionClass> list = new List<SessionClass>();
+            list.Add(await db.GetSessionByID(ViewSessionTeacherAccess.sessionIDforteacher));
+            collectionView.ItemsSource = list;
+
+        }
+        public void btnOpen_Clicked(object sender, EventArgs e)
+        {
+            string temp = NewGrade.Text;
+            List<SessionClass> list = new List<SessionClass>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].sessionID.Equals(ViewSessionTeacherAccess.sessionIDforteacher))
+                {
+                    list[i].grade = temp;
+                    nav();
+                }
+            }
+        }
+
+        void goHome(object sender, EventArgs e)
+        {
+            nav();
+        }
+
+        async void nav()
+        {
+            await Navigation.PushAsync(new HomeTeacher());
+        }
+
+    }
+}
--- TutorManagementiOS/TutorManagementiOS/ViewSessionTeacherAccess.xaml.cs
+++ TutorManagementiOS/TutorManagementiOS/ViewSessionTeacherAccess.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TutorManagementiOS.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,7 +12,7 @@ namespace TutorManagementiOS
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ViewSessionTeacherAccess : ContentPage
     {
-        public static string sessionID;
+        public static string sessionIDforteacher;
         FirebaseRepo db = new FirebaseRepo();
         public ViewSessionTeacherAccess()
         {
@@ -22,33 +22,57 @@ namespace TutorManagementiOS
         async void displaySessions()
         {
             List<SessionClass> list = await db.GetAllSessions();
-            List<SessionClass> filtered = new List<SessionClass>();
-            for (int i = 0; i < list.Count; i++)
+            List<SessionClass> listgood = new List<SessionClass>();
+            List<UserClass> listeroo = await db.GetAllUsers();
+            List<CourseMemberClass> lister = await db.GetAllCoursesMembers();
+            List<CourseClass> listering = await db.GetAllCourses();
+            List<Teacher> listing1 = await db.GetAllTeachers();
+            for (int i = 0; i < listeroo.Count; i++)
             {
-                if(list[i].completed == false)
+                for (int a = 0; a < listing1.Count; a++)
                 {
-                    filtered.Add(list[i]);
+                    if (listing1[a].genUserID.Equals(listeroo[i].userID))
+                    {
+                        for (int h = 0; h < lister.Count; h++)
+                        {
+                            if (lister[h].userID.Equals(listeroo[i].userID))
+                            {
+                                for (int p = 0; p < listering.Count; p++)
+                                {
+                                    if (listering[p].courseID.Equals(lister[h].courseID))
+                                    {
+                                        for (int g = 0; g < list.Count; g++)
+                                        {
+                                            if (list[g].course.Equals(listering[p].courseName) & list[g].completed==true)
+                                            {
+                                                listgood.Add(list[g]);
+                                            }
+                                        }
+                                    }
+                                }   
+                            }
+                        }
+                    }
                 }
             }
-            
-            if (filtered.Any())
+            if (listgood.Any())
             {
-                collectionView.ItemsSource = filtered;
+                collectionView.ItemsSource = listgood;
             }
             else
             {
                 _ = DisplayAlert("Error", "DB empty...", "ok");
             }
         }
-        async void goDetail(object sender, CheckedChangedEventArgs e)
+        async void goDetail(object sender, EventArgs e)
         {
-            var radiobtn = sender as RadioButton;
-            sessionID = (string)radiobtn.Content;
+            var button = sender as Button;
+            sessionIDforteacher = (string)button.Text;
             List<SessionClass> list = await db.GetAllSessions();
 
             for (int i = 0; i < list.Count; i++)
             {
-                if (list[i].sessionID.Equals(sessionID))
+                if (list[i].sessionID.Equals(sessionIDforteacher))
                 {
 
                     nav();
@@ -60,7 +84,7 @@ namespace TutorManagementiOS
         }
         async void nav()
         {
-            await Navigation.PushAsync(new SessionDetailTeacherAccess()); ;
+            await Navigation.PushAsync(new SessionGradingPage()); ;
         }
 
     }
